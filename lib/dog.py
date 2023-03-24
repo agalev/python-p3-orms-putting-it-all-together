186684c9a5b363466161fe55a5a96602aa38b912
Alexander <alex.galev@yahoo.com>
2023-03-24 11:55:43 -0600

    no bonuses

--- lib/dog.py
+++ lib/dog.py
@@ -4,5 +4,82 @@ CONN = sqlite3.connect('lib/dogs.db')
 CURSOR = CONN.cursor()
 
 class Dog:
+    def __init__(self, name, breed):
+        self.id = None
+        self.name = name
+        self.breed = breed
+
+    @classmethod
+    def create_table(cls):
+        CURSOR.execute("""
+            CREATE TABLE IF NOT EXISTS dogs (
+                id INTEGER PRIMARY KEY,
+                name TEXT,
+                breed TEXT
+            )
+        """)
+        CONN.commit()
     
-    pass
+    @classmethod
+    def drop_table(cls):
+        sql = "DROP TABLE IF EXISTS dogs"
+        CURSOR.execute(sql)
+    
+    def save(self):
+        sql = "INSERT INTO dogs (name, breed) VALUES (?, ?)"
+        CURSOR.execute(sql, (self.name, self.breed))
+        CONN.commit()
+        self.id = CURSOR.lastrowid
+    
+    @classmethod
+    def create(cls, name, breed):
+        dog = cls(name, breed)
+        dog.save()
+        return dog
+    
+    @classmethod
+    def new_from_db(cls, row):
+        NewDog = Dog(row[1], row[2])
+        NewDog.id = row[0]
+        return NewDog
+
+    @classmethod
+    def get_all(cls):
+        sql = "SELECT * FROM dogs"
+        CURSOR.execute(sql)
+        rows = CURSOR.fetchall()
+        return [cls.new_from_db(row) for row in rows]
+        # result = []
+        # for row in rows:
+        #     result.append(cls.new_from_db(row))
+        # print(rows)
+        # return rows
+        # print(result[0].name)
+        # return to_return
+    @classmethod
+    def find_by_name(cls, name):
+        sql = "SELECT * FROM dogs WHERE name = ?"
+        CURSOR.execute(sql, (name,))
+        row = CURSOR.fetchone()
+        return cls.new_from_db(row)
+    
+    @classmethod
+    def find_by_id(cls, id):
+        sql = "SELECT * FROM dogs WHERE id = ?"
+        CURSOR.execute(sql, (id,))
+        row = CURSOR.fetchone()
+        return cls.new_from_db(row)
+    
+    def find_or_create_by(self, name, breed):
+        dog = self.find_by_name(name)
+        if dog:
+            return dog
+        else:
+            self.create(name, breed)    
+    
+    def update(self):
+        sql = "UPDATE dogs SET name = ?, breed = ? WHERE id = ?"
+        CURSOR.execute(sql, (self.name, self.breed, self.id))
+        CONN.commit()
+# Dog.get_all()
+# import ipdb; ipdb.set_trace()
